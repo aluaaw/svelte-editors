32600205817e92422cb9073883d68d26ecc36fed
aluaaw <ylua.aw@gmail.com>
2022-09-06 18:01:26 +0900

    init editors

---
+++ src/routes/editorjs/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { createEditor } from 'svelte-editorjs'
+	const { editor, data, isReady } = createEditor()
+	
+	$: {
+		if (isReady) {
+			console.log('Saved data:', $data)
+		}
+	}
+</script>
+
+<h1>EditorJS</h1>
+<div class=editor use:editor />
+<button on:click={() => $editor.clear()}>Clear contents</button>
+<button on:click={() => $editor.save()}>Save contents</button>
+
+<style>
+	.editor {
+		border: 2px solid #ccc;
+	}
+</style>
